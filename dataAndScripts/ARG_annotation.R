0d7824906fada8203ebffeb2cdab19a13acf14ac
VanCamp <Pieter-Jan.VanCamp@cchmc.org>
2021-02-10 09:30:34 -0500

    Finally some promising results :)

--- dataAndScripts/ARG_annotation.R
+++ dataAndScripts/ARG_annotation.R
@@ -6,43 +6,27 @@ library(gfaTools)
 library(tidyverse)
 library(RSQLite)
 
-tempFolder = formatPath("temp/", endWithSlash = T)
-sampleName = "SRR3170779-SRR4065677_0.5-0.5_1611953376"
-minBlastLength = 250
+#Get the arguments
+tempFolder = formatPath("temp/", endWithSlash = F)
+pipelineIds = NULL
 
+#Load the ARG and the sample list to process
 myConn = dbConnect(SQLite(), "dataAndScripts/meta2amr.db")
+
 ARG = dbReadTable(myConn, "ARG") %>% 
   mutate(geneId = as.character(geneId))
+
+toProcess = dbReadTable(myConn, "pipeline") %>% 
+  filter(statusCode == 4)
+if(length(pipelineIds) > 0){
+  toProcess = toProcess %>% filter(pipelineId %in% pipelineIds)
+}
+
 dbDisconnect(myConn)
-
-tempNames = tbl(myConn, "blastSubmissions") %>% 
-  select(tempName) %>% distinct() %>% pull(tempName)
-
-# ### FIX mismatch runId in blast prep!! 
-# genesDetected = dbGetQuery(
-#   myConn, 
-#   "SELECT a.runId, a.segmentLength, a.kmerCount, a.n, a.coverage, c.*
-#   FROM detectedARG a, blastSubmissions as b, ARG as c
-#   WHERE a.runId == 27 AND b.runId == 28 AND 
-#   b.tempName == 'testMix2WithBG_1605708377' AND c.geneId = a.geneId") %>% 
-#   distinct()
-# dbDisconnect(myConn)
 
 
 # ---- FUNCTIONS ----
 #********************
-#Get the mean based on the max (default) at each position in the coverage
-coverMean = function(from, to, n, val, fun = max){
-  apply(
-    mapply(function(from, to, n, val, fun){
-      x = rep(0.0, n)
-      x[from:to] = val
-      return(x)
-    }, from, to, n, val),
-    1, fun
-  ) %>% mean()
-}
-
 cutOff = function(numbers){
   if(length(numbers) < 2){
     return(numbers)
@@ -53,29 +37,18 @@ cutOff = function(numbers){
   myData %>% filter(diff == max(diff)) %>% pull(number) %>% unique() %>% min()
 }
 
-#D5Heidi011_SRR3981085_0.056_1610245775 KA/P
-#D5Heidi011_SRR3981085_0.056_1611844923 v2
-#D7Heidi5_SRR3987134_0.01_1611882674 KA - after gene fix
-#G7Heidi5_SRR5120224_0.076_1610200963  EC
-#H6Heidi4_SRR2449036_0.01_1611879358 EC - after gene fix
-#SRR3170779-SRR4065677_0.5-0.5_1611953376 - mix AB and EC
-# G8Heidi010_SRR4017917_0.1_1611877967 - EClo
-# D5Heidi011_SRR4017818_0.01_1611876760 - EClo
-sampleName = "D5Heidi011_SRR13481113_0.05_1612457271"
-genesDetected = read_csv(
-  paste0(tempFolder, sampleName,
-         "/genesDetected/genesDetected.csv"))
-
-result = map_df(sampleName, function(sampleName){
+sample = "temp/meta2amrPipeline_qQRvE_1612914889"
+for(sample in toProcess$tempFolder){
   
-  print(sampleName)
+  sampleName = str_extract(sample, "[^\\/]+(?=_\\d+$)")
+  print(paste("Annotating the genes for", sampleName))
   
   # ---- FILTERING DATA ----
   #*************************
   
   #Read all blast output
   blastOut = lapply(
-    list.files(paste0(tempFolder, sampleName), full.names = T,
+    list.files(sample, full.names = T,
                pattern = "blastSegmentsClustered\\d+.json.gz"),
     blast_readResults, outFormat = "dataFrame1") %>% bind_rows()
   
@@ -91,7 +64,7 @@ result = map_df(sampleName, function(sampleName){
     mutate(plasmid = str_detect(extra, "plasmid|Plasmid"))
   
   #Expand results from clustering segments before blast
-  clusterOut = read.table(paste0(tempFolder, sampleName, "/blastSegments.out")) %>% 
+  clusterOut = read.table(paste0(sample, "/blastSegments.out")) %>% 
     select(segmentId = V9, clusterId = V10) %>% distinct() %>% 
     mutate(clusterId = ifelse(clusterId == "*", segmentId, clusterId)) %>% 
     extract(segmentId, into = c("geneId", "segment"), regex = "^([^_]+)_(.*)", remove = F) %>% 
@@ -102,7 +75,7 @@ result = map_df(sampleName, function(sampleName){
   
   
   #Get the KC and LN for the segments
-  segmentInfo =  read_csv(paste0(tempFolder, sampleName, "/blastSegments.csv")) %>% 
+  segmentInfo =  read_csv(paste0(sample, "/blastSegments.csv")) %>% 
     select(-sequence, -name, -geneId, -CL)
   
   #Check the bacteria
@@ -127,12 +100,11 @@ result = map_df(sampleName, function(sampleName){
     filter(depth <= depth[bit_score == max(bit_score)])
   
   #Get all data from the paths
-  allGFA = list.files(
-    paste0(tempFolder, sampleName, "/genesDetected/simplifiedGFA"), 
+  pathData = list.files(
+    paste0(sample, "/genesDetected/simplifiedGFA"), 
     pattern = ".gfa", full.names = T)
   
-  pathData = map_df(allGFA, function(myGFA){
-    print(which(allGFA == myGFA))
+  pathData = map_df(pathData, function(myGFA){
     gfa = gfa_read(myGFA)
     segmentOfInterest = gfa$segments %>% filter(str_detect(name, "_start$")) %>%
       filter(LN == max(LN)) %>% filter(KC == max(KC)) %>% slice(1) %>% pull(name)
@@ -150,7 +122,7 @@ result = map_df(sampleName, function(sampleName){
   })
   
   #Get the result
-  test = 
+  blastOut = 
     blastOut %>% 
     filter(
       extra != "",
@@ -177,107 +149,6 @@ result = map_df(sampleName, function(sampleName){
     left_join(ARG %>% select(geneId, gene, subtype, clusterNr), by = "geneId") %>% 
     select(gene, subtype, clusterNr, accession, genus, species, extra, plasmid, val) %>% distinct() %>% 
     group_by(gene, subtype) %>%  filter(val >= cutOff(val))
-})
 
-write_csv(result, "testAssignGenes.csv")
-
-
-
-#---------------
-#It seems that using multiple matches within the same genome does not make sense,
-#since it would not have been split if it would be extended with a gap.
-#So better get rid of all but the best match? 
-# We also have to work by hit Id before we start merging
-#  group_by(clusterId, hitId)
-### Here starts speculation ###
-
-#Use the covermean function so summarise data per gene, bact and segment
-test = blastOut %>% group_by(hitId, geneId, genus, species, segment, plasmid) %>% 
-  summarise(
-    # score_mean = coverMean(query_from, query_to, query_len, score, max),
-    bit_score = coverMean(query_from, query_to, query_len, bit_score),
-    # evalue = coverMean(query_from, query_to, query_len, evalue, 
-                       # fun = function(x) ifelse(sum(x) == 0, 0, min(x[x != 0]))),
-    coverage = coverMean(query_from, query_to, query_len, 1.0),
-    # n = coverMean(query_from, query_to, query_len, n()),
-    query_len = query_len[1], .groups = "drop")
-
-#Get the top for each gene
-test1 = test %>% group_by(geneId, segment) %>% 
-  filter(bit_score == max(bit_score)) %>%  filter(coverage > 0.9) %>% 
-  select(-hitId) %>% distinct()
-
-# test1 = test %>% group_by(geneId, segment) %>% %>% group_by(geneId, genus, species, plasmid) %>% 
-#   summarise(bit_score = sum(bit_score), coverage = mean(coverage), n = n(), query_len = sum(query_len)) %>% 
-#   arrange(geneId, desc(bit_score)) %>% group_by(geneId) %>%  top_n(2)
-
-
-#Create a coloured gfa
-segmentData = test %>% 
-  mutate(info = paste(str_extract(genus, "..."), 
-                      str_extract(species, "..."),
-                      as.integer(bit_score),
-                      collapse = " ")) %>%  
-  group_by(geneId, segment) %>% 
-  summarise(annot = paste(info, collapse = "; "), .groups = "drop")
-
-myGene = "2148"
-
-myGFA = gfa_read(sprintf("%s%s/genesDetected/simplifiedGFA/%s_simplified.gfa", 
-                         tempFolder, sampleName, myGene))
-myGFA = gfa_annotation(
-  myGFA, 
-  segments = segmentData %>% filter(geneId == myGene) %>% pull(segment), 
-  label = segmentData %>% filter(geneId == myGene) %>% pull(annot))
-
-# gfa_write(myGFA, "D:/Desktop/testGFA.gfa")
-
-#Use the path table to see if you can reconstruct path for bacterium all the way to start
-start = myGFA$segments %>% filter(str_detect(name, "_start$")) %>% 
-  filter(LN == max(LN)) %>% pull(name)
-paths = gfa_pathsToSegment(myGFA, start, returnList = T)
-names(paths) = sapply(paths, "[", "endSegment")
-
-# result = paths[test %>% filter(geneId == myGene) %>% pull(segment)]
-
-result = map_df(paths, function(myPath){
-  # segmentOrder = myPath$segmentOrder[!str_detect(myPath$segmentOrder, "start$")]
-  segmentOrder = myPath$segmentOrder
-  if(length(segmentOrder) > 0){
-    data.frame(step = 1:length(segmentOrder), segment = segmentOrder) %>% 
-      left_join(test %>% 
-                  filter(geneId == myGene), by = "segment") %>% 
-      left_join(myGFA$segments %>% 
-                  select(name, LN, KC), by = c("segment" = "name")) %>% 
-      mutate(endSegment = myPath$endSegment,
-             pathLength = myPath$pathLength)
-  } else {
-    data.frame()
-  }
-}) 
-# %>% group_by(endSegment) %>% 
-#   mutate(pathLength = pathLength[1] - sum(LN[is.na(geneId)]) + 60*n() - 60)
-
-#Filter by minBlastLength
-# test1 = result %>% 
-#   # mutate(LN = LN - 30) %>% 
-#   # filter(LN >= minBlastLength) %>%
-#   group_by(endSegment, genus, species) %>% 
-#   mutate(pathLength = pathLength - sum(LN[is.na(geneId)])) %>% 
-#   summarise(bit_score = sum(bit_score), n = n(), 
-#             pathAssigned = sum(LN) / pathLength[1],
-#             .groups = "drop")
-
-
-test1 = result %>% filter(!is.na(geneId)) %>% 
-  mutate(unique = ifelse(segment == endSegment, LN / pathLength, 0)) %>% 
-  group_by(endSegment, genus, species, plasmid) %>% mutate(n = n()) %>% 
-  group_by(endSegment, plasmid) %>% filter(n == max(n)) %>% 
-  group_by(endSegment, genus, species, plasmid)  %>% 
-  summarise(bit_score = sum(bit_score), 
-            unique = max(unique), 
-            quality = weighted.mean(coverage, LN) * pathLength,
-            .groups = "drop") %>% 
-  filter(unique > 0)
-
-genesDetected %>% filter(geneId == myGene)
+    write_csv(blastOut, paste0(sample, "/annotation.csv"))
+}
