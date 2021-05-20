dd88fe37f25e3dbc9e6a3e9699ed057b5241736f
pieterjanvc <pieterjanvc@hotmail.com>
2021-05-20 16:09:51 -0400

    Implement a limit for the distance from the seed gene for which metacherchant assembles

--- meta2amr.sh
+++ meta2amr.sh
@@ -258,6 +258,7 @@ if [ -z "$MCsuccess" ]; then
 		--coverage=5 \
 		--reads $inputFile \
 		--seq $baseFolder/dataAndScripts/ARG_06Jan2020.fasta \
+		--maxradius 2500 \
 		--output $tempFolder/$tempName \
 		--work-dir $tempFolder/$tempName/metacherchant_logs \
 		--maxkmers=100000 \
