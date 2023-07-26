d54787b06ef263a36803e19c150c5b10e1029f70
PJ <52793278+pieterjanvc@users.noreply.github.com>
2023-07-26 19:06:32 -0400

    comparabe outputs for mgs and sp pipeline

--- revisions/revisions_analysis.R
+++ revisions/revisions_analysis.R
@@ -7,28 +7,35 @@ library(RSQLite)
 
 #Databases 
 supplementalDB = "D:/Desktop/SupplementalDatabase.db"
-resultsDB = "D:/Desktop/results.db"
-revisionsDB = "D:/Desktop/revisions.db"
+metaspadesDB = "D:/Desktop/results.db"
+mgs2amspDB = "D:/Desktop/revisions.db"
 
-#Limit analysis to the following pipelineIds (or c() for all)
-pipelineIds = c()
+#Limit analysis to the following spPipeIds (or c() for all)
+spPipeIds = c()
 
 
 sDB = dbConnect(dbDriver("SQLite"), supplementalDB)
-rDB = dbConnect(dbDriver("SQLite"), resultsDB)
-vDB = dbConnect(dbDriver("SQLite"), revisionsDB)
+spDB = dbConnect(dbDriver("SQLite"), metaspadesDB)
+mgsDB = dbConnect(dbDriver("SQLite"), mgs2amspDB)
 
 #Get the results
-info = tbl(rDB, "pipeline") %>%  collect()
+info = tbl(spDB, "pipeline") %>%  collect() %>% 
+  rename(spPipeId = pipelineId)
 
-if(length(pipelineIds) == 0){
-  pipelineIds = info$pipelineId
+if(length(spPipeIds) == 0){
+  spPipeIds = info$spPipeId
 } else {
-  info = info %>% filter(pipelineId %in% {{pipelineIds}})
+  info = info %>% filter(spPipeId %in% {{spPipeIds}})
 }
 
-argInfo = tbl(rDB, "argInfo") %>% 
-  filter(pipelineId %in% local(pipelineIds)) %>% collect()
+argInfo = tbl(spDB, "argInfo") %>% 
+  filter(pipelineId %in% local(spPipeIds)) %>% collect() %>% 
+  rename(spPipeId = pipelineId)
+
+mgs2amrInfo = tbl(mgsDB, "pipeline") %>% filter(name %in% local(info$name)) %>% 
+  filter(statusCode == 5) %>% collect() %>% 
+  left_join(info %>% select(name, spPipeId), by = "name") %>% 
+  rename(mgsPipeId = pipelineId)
 
 #---------------- REMOVE NEXT RUN!!!
 overlap = function(x, y, amount = 50){
@@ -58,81 +65,103 @@ argInfo = argInfo %>% group_by(qseqid) %>%
 genotypes = tbl(sDB, "genotypes") %>% 
   filter(Run %in% local(info$isolate)) %>% 
   filter(hasLink == 1) %>% 
-  # left_join(tbl(myConn, "geneLinks")) %>% 
-  collect()
+  collect() %>% rename(isolate = Run) %>% 
+  left_join(info %>% select(spPipeId, isolate), by = "isolate",
+            relationship = "many-to-many") %>% 
+  left_join(mgs2amrInfo %>% select(spPipeId, mgsPipeId), by = "spPipeId")
+
 bactInfo = tbl(sDB, "sampleInfo") %>% 
   filter(Run %in% local(info$isolate)) %>% collect() %>% 
-  left_join(info %>% select(pipelineId, Run = isolate), by = "Run")
+  left_join(info %>% select(spPipeId, Run = isolate), by = "Run") %>% 
+  left_join(mgs2amrInfo %>% select(spPipeId, mgsPipeId), by = "spPipeId")
+
 ARG = tbl(sDB, "ARG") %>% collect()
 
 #Get more results...
-scaffolds = tbl(rDB, "scaffold") %>% 
-  filter(pipelineId %in% local(pipelineIds)) %>% collect()
-blastOut = tbl(rDB, "blastOut") %>% 
-  filter(pipelineId %in% local(pipelineIds)) %>% collect() %>% 
+scaffolds = tbl(spDB, "scaffold") %>% 
+  filter(pipelineId %in% local(spPipeIds)) %>% collect() %>% 
+  rename(spPipeId = pipelineId)
+blastOut = tbl(spDB, "blastOut") %>% 
+  filter(pipelineId %in% local(spPipeIds)) %>% collect() %>% 
   mutate(geneId = str_extract(qseqid, "\\d+$") %>% as.integer) %>% 
   left_join(ARG %>% select(geneId, gene), by = "geneId") %>% 
-  left_join(bactInfo %>% select(pipelineId, Bacterium), 
-            by = c("pipelineId"))
+  rename(spPipeId = pipelineId) %>% 
+  left_join(bactInfo %>% select(spPipeId, Bacterium),
+            by = c("spPipeId"))
 
 #Get MGS2AMR results
-mgs2amrInfo = tbl(vDB, "pipeline") %>% filter(name %in% local(info$name)) %>% 
-  filter(statusCode == 5) %>% collect() %>% 
-  left_join(info %>% select(revPipeId = pipelineId, name), by = "name")
-detectedARG = tbl(vDB, "detectedARG") %>% 
-  filter(pipelineId %in% local(mgs2amrInfo$pipelineId)) %>% collect() %>% 
-  left_join(ARG %>% select(geneId, gene, subtype), by = "geneId")
-annotation = tbl(vDB, "annotation") %>% 
-  filter(pipelineId %in% local(mgs2amrInfo$pipelineId)) %>% 
-  left_join(tbl(vDB, "bactStrains"), by = "accession") %>% 
-  left_join(tbl(vDB, "bactTaxa"), by = "taxid") %>% 
-  mutate(isolate = paste(genus, species)) %>% collect() %>% 
-  left_join(mgs2amrInfo %>% select(pipelineId, revPipeId), by = "pipelineId") %>% 
-  left_join(bactInfo %>% select(revPipeId = pipelineId, Bacterium),
-            by = "revPipeId")
+# mgs2amrInfo = tbl(mgsDB, "pipeline") %>% filter(name %in% local(info$name)) %>% 
+#   filter(statusCode == 5) %>% collect() %>% 
+#   left_join(info %>% select(revPipeId = pipelineId, name), by = "name")
+detectedARG = tbl(mgsDB, "detectedARG") %>% 
+  filter(pipelineId %in% local(mgs2amrInfo$mgsPipeId)) %>% collect() %>% 
+  left_join(ARG %>% select(geneId, gene, subtype), by = "geneId") %>% 
+  rename(mgsPipeId = pipelineId)
 
-dbDisconnect(rDB)
+annotation = tbl(mgsDB, "annotation") %>% 
+  filter(pipelineId %in% local(mgs2amrInfo$mgsPipeId)) %>% 
+  left_join(tbl(mgsDB, "bactStrains"), by = "accession") %>% 
+  left_join(tbl(mgsDB, "bactTaxa"), by = "taxid") %>% 
+  mutate(isolate = paste(genus, species)) %>% collect() %>% 
+  rename(mgsPipeId = pipelineId) %>% 
+  # left_join(mgs2amrInfo %>% select(mgsPipeId, revPipeId), by = "pipelineId") %>% 
+  left_join(bactInfo %>% select(mgsPipeId, Bacterium),
+            by = "mgsPipeId")
+
+dbDisconnect(spDB)
 dbDisconnect(sDB)
-dbDisconnect(vDB)
+dbDisconnect(mgsDB)
 
 #Don't use subtypes for gene matching
-genotypes = genotypes %>% select(isolate = Run, subtype = gene) %>% 
+genotypes = genotypes %>% 
+  select(isolate, subtype = gene, type, core, spPipeId, mgsPipeId) %>% 
   left_join(ARG %>% select(gene, subtype), by = c("subtype")) %>% 
   mutate(gene = ifelse(is.na(gene), subtype, gene)) %>% 
   select(-subtype) %>% distinct()
 
+# test = info %>% select(pipelineId, isolate) %>% 
+#   left_join(genotypes %>% select(isolate, gene), by = "isolate",
+#             relationship = "many-to-many") %>% mutate(match = T) %>% 
+#   left_join(mgs2amrInfo %>% select(pipelineId, revPipeId),
+#             by = "pipelineId")
+
 
 #------------------ MGS2AMR
-test = annotation %>% group_by(pipelineId, geneId, genus, species) %>% 
+mgsResult = annotation %>% group_by(mgsPipeId, geneId, genus, species) %>% 
   filter(fullPath == max(fullPath)) %>% slice(1) %>% 
-  group_by(pipelineId, geneId) %>% 
+  group_by(mgsPipeId, geneId) %>% 
   mutate(
     rank = rank(1/fullPath, ties.method = "min"),
     singleTop = sum(rank == 1) == 1,
     match = isolate == Bacterium
   ) %>% ungroup() %>% filter(match)
 
-test = detectedARG %>% left_join(test %>% 
-  select(revPipeId, pipelineId, geneId, extension, fullPath, isolate, rank:match),
-    by = c("pipelineId", "geneId")) %>% 
- full_join(genotypes)
+mgsResult = detectedARG %>% left_join(mgsResult %>% 
+  select(mgsPipeId, geneId, extension, fullPath, isolate, rank:match),
+    by = c("mgsPipeId", "geneId")) %>% 
+  full_join(genotypes %>% select(gene, mgsPipeId, core) %>% 
+              filter(!is.na(mgsPipeId)), by = c("gene", "mgsPipeId")) %>% 
+  mutate(fullPath = replace_na(fullPath, 0)) %>% 
+  group_by(mgsPipeId, ARGgroup) %>% filter(fullPath == max(fullPath))
 
-#------------- VALIDATION
+#------------- SPADES VALIDATION
 
 #Analysis
-test1 = argInfo %>% select(pipelineId, gene, coverage, qseqid, gene, bitscore_gene = bitscore, grp) %>% distinct() %>% 
-  left_join(info %>% select(pipelineId, isolate), by = "pipelineId") %>% 
+test1 = argInfo %>% select(spPipeId, gene, coverage, qseqid, gene, 
+                           bitscore_gene = bitscore, grp) %>% distinct() %>% 
+  left_join(info %>% select(spPipeId, isolate), by = "spPipeId") %>% 
   mutate(detection = "XP") %>% 
-  group_by(pipelineId, qseqid, grp) %>% 
+  group_by(spPipeId, qseqid, grp) %>% 
   #Use both cov and bit score to pick best
   filter(coverage * bitscore_gene == max(coverage * bitscore_gene)) %>% 
   ungroup()
 
-test2 = info %>% select(pipelineId, isolate) %>% 
-  left_join(genotypes %>% select(isolate, gene), by = "isolate",
-            relationship = "many-to-many") %>% mutate(match = T)
+# test2 = info %>% select(pipelineId, isolate) %>% 
+#   left_join(genotypes %>% select(isolate, gene), by = "isolate",
+#             relationship = "many-to-many") %>% mutate(match = T)
 
-result = test1 %>% full_join(test2, by = c("pipelineId", "isolate", "gene")) %>% 
+spResult = test1 %>% full_join(genotypes %>% mutate(match = T), 
+                             by = c("spPipeId", "isolate", "gene")) %>% 
   mutate(
     detection = replace_na(detection, "FN"),
     detection = case_when(
@@ -146,28 +175,29 @@ result = test1 %>% full_join(test2, by = c("pipelineId", "isolate", "gene")) %>%
     coverage = replace_na(coverage, 0),
     qseqid = replace_na(qseqid, "not detected")
   ) %>% 
-  group_by(pipelineId, qseqid, grp) %>% filter(coverage == max(coverage)) %>% 
+  group_by(spPipeId, qseqid, grp) %>% filter(coverage == max(coverage)) %>% 
   ungroup()
 
 
-result = result %>% left_join(
+spResult = spResult %>% left_join(
   blastOut %>% 
-    select(pipelineId, gene, qseqid, sscinames, Bacterium, rank, 
+    select(spPipeId, gene, qseqid, sscinames, Bacterium, rank, 
            bitscore_bact = bitscore, singleTop) %>%
     filter(sscinames == Bacterium) %>% 
     mutate(qseqid = str_remove(qseqid, "_\\d+$")) %>% distinct(), 
-  by = c("pipelineId", 'gene', "qseqid"))
+  by = c("spPipeId", 'gene', "qseqid"))
 
 
-result = result %>% filter(!is.na(sscinames) | detection == "FN") %>% 
+spResult = spResult %>% filter(!is.na(sscinames) | detection == "FN") %>% 
   #COuld be useful
   # filter(coverage > 0.9 | detection == "FN") %>% 
-  arrange(pipelineId, gene, bitscore_gene)
+  arrange(spPipeId, gene, bitscore_gene)
 
-result %>% group_by(pipelineId) %>% summarise(
+spResult %>% group_by(spPipeId) %>% summarise(
   nGenes = sum(detection %in% c("TP","FN")), detected = sum(detection == "TP"),
   perc = detected / nGenes
 )
 
 ### NEED TO FIND A WAY FOR OTHER FP GENES
-
+utils::browseURL(sprintf("https://www.ncbi.nlm.nih.gov/pathogens/isolates#%s", 
+                         bactInfo$Run[1]))
